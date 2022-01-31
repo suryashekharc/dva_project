e3a2985f1425d277cf6151292266ec07bc10c4f5
Surya Shekhar Chakraborty <8998817+suryashekharc@users.noreply.github.com>
2022-01-31 03:00:15 -0500

    Print the structure of a reddit submission object from r/soccer

--- data_collection/scrape_reddit.py
+++ data_collection/scrape_reddit.py
@@ -5,16 +5,16 @@ https://github.com/suryashekharc/insta_reddit/blob/master/insta_reddit/code/down
 """
 import argparse
 import sys
-from pathlib import Path
 
 import pandas as pd
 import praw
-from insta_reddit import credentials
-from insta_reddit.code.sheets_db import SheetsDb  # To append records to sheets
+from print_schema import print_schema
+import credentials
 
 
 def initialize():
     # NOTE: Ensure the credentials.py file is present in the current directory
+    # and not committed to the repo
     reddit_obj = praw.Reddit(client_id=credentials.client_id,
                              client_secret=credentials.client_secret,
                              user_agent=credentials.user_agent,
@@ -25,7 +25,7 @@ def initialize():
 
 
 def get_posts(reddit_obj,
-              subreddit_name="unethicallifeprotips",
+              subreddit_name="soccer",
               post_count=20,
               time_filter="month",
               fields=None):
@@ -38,10 +38,11 @@ def get_posts(reddit_obj,
     :param fields:          List of fields to return (Refer: sample_object_json.txt)
     :return:                Pandas DF with "count" rows and "len(kwargs)" columns
     """
-    # TODO: Add support for hot along with top
-
     if fields is None:  # To avoid passing mutable default args
-        fields = ["title", "selftext", "author", "url", "id"]
+        fields = [
+            "author", "comments", "created_utc", "id", "link_flair_template_id",
+            "link_flair_text", "name", "num_comments", "score", "selftext",
+            "title", "upvote_ratio", "url"]
     if "id" not in fields:  # To ensure the unique ID of a post is always captured
         fields.append("id")
     content_dict = {field: [] for field in fields}  # Initializing dict
@@ -50,8 +51,11 @@ def get_posts(reddit_obj,
     # https://praw.readthedocs.io/en/latest/code_overview/models/submission.html
     # PRAW uses lazy objects so that network requests to ...
     # ... Reddit's API are only issued when information is needed.
-    for submission in reddit_obj.subreddit(subreddit_name).top(limit=post_count,
-                                                               time_filter=time_filter):
+    for submission in reddit_obj.subreddit(subreddit_name).top(
+        limit=post_count, time_filter=time_filter):
+        print(submission.title)  # to make it non-lazy
+        print_schema(vars(submission))
+        return
         for field in fields:
             content_dict[field].append(getattr(submission, field))
     return pd.DataFrame(content_dict)
@@ -63,7 +67,8 @@ def main(args):
                            str(args.subreddit_name),
                            int(args.post_count),
                            str(args.time_filter),
-                           args.fields.replace(" ", "").split(","))
+                           args.fields)
+    print(content_df)
     # TODO: Save content_df as pandas pickle object for now
 
 if __name__ == "__main__":
@@ -74,7 +79,7 @@ if __name__ == "__main__":
                         help="""Number of posts to fetch""")
     parser.add_argument('--time_filter', dest='time_filter', default="month",
                         help="""day/month/week etc since we are sorting by top""")
-    parser.add_argument('--fields', dest='fields', default="title,selftext,author,url,id",
+    parser.add_argument('--fields', dest='fields', default=None,
                         help="""Comma-separated list of fields to save""")
 
     main(args=parser.parse_args())
